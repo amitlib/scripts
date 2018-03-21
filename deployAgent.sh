dcdc2d4a4327040adf69463b189f2bcb0ee8ed6d
amitlib <31591675+amitlib@users.noreply.github.com>
2018-03-21 17:52:57 +0200

    Update deployAgent.sh

--- deployAgent.sh
+++ deployAgent.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 SERVER_IP="${1:-10.0.0.5}"
-AQUA_REPO="${2:-aquadev}"
-AQUA_VERSION="${3:-3.0.1}"
+DOCKER_PASS=$2
+DOCKER_USER=$3
+AQUA_REPO="${4:-aquadev}"
+AQUA_VERSION="${5:-3.0.1}"
+
+touch /home/ubuntu/scripts/logs/extension.log
+chmod 777 /home/ubuntu/scripts/logs/extension.log
+echo "SERVER_IP: $SERVER_IP" >> /home/ubuntu/scripts/logs/extension.log
+echo "AQUA_REPO: $AQUA_REPO" >> /home/ubuntu/scripts/logs/extension.log
+echo "AQUA_VERSION: $AQUA_VERSION" >> /home/ubuntu/scripts/logs/extension.log
+echo "DOCKER_PASS: $DOCKER_PASS" >> /home/ubuntu/scripts/logs/extension.log
+BABA=$(echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin docker.io)
 
 cd /home/ubuntu/scripts
 
