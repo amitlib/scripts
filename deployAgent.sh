7b065bcd998c2950b2d17b81438d3506b54865a8
amitlib <31591675+amitlib@users.noreply.github.com>
2018-03-22 14:40:19 +0200

    Added cadvisor to init script

--- deployAgent.sh
+++ deployAgent.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
+#Globals
+echo "step start: globals"
 SERVER_IP="${1:-10.0.0.5}"
 DOCKER_PASS=$2
 DOCKER_USER=$3
 AQUA_REPO="${4:-aquadev}"
 AQUA_VERSION="${5:-3.0.1}"
+echo "step end: globals"
 
+#Pre config
+echo "step start: pre-config"
 touch /home/ubuntu/scripts/logs/extension.log
 chmod 777 /home/ubuntu/scripts/logs/extension.log
+echo "step end: pre-config"
+
+#Validations
+echo "step start: validations"
 echo "SERVER_IP: $SERVER_IP" >> /home/ubuntu/scripts/logs/extension.log
 echo "AQUA_REPO: $AQUA_REPO" >> /home/ubuntu/scripts/logs/extension.log
 echo "AQUA_VERSION: $AQUA_VERSION" >> /home/ubuntu/scripts/logs/extension.log
 echo "DOCKER_PASS: $DOCKER_PASS" >> /home/ubuntu/scripts/logs/extension.log
 BABA=$(echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin docker.io)
+echo "step end: validations"
 
-cd /home/ubuntu/scripts
+cd /home/$(whoami)/scripts
+#Run Cadvisor
+echo "step start: cadvisor"
+docker run --volume=/:/rootfs:ro --volume=/var/run:/var/run:rw --volume=/sys:/sys:ro \
+--volume=/var/lib/docker/:/var/lib/docker:ro --volume=/dev/disk/:/dev/disk:ro -p 8090:8080 --detach=true \
+--name=cadvisor google/cadvisor:latest
+echo "step end: cadvisor"
 
+#Run Aqua Agent
+echo "step start: aqua agent"
 docker run --rm -e SILENT=yes \
 -e AQUA_TOKEN=agent-scale-token \
 -e AQUA_SERVER=${SERVER_IP}:3622 \
@@ -23,3 +41,4 @@ docker run --rm -e SILENT=yes \
 -e RESTART_CONTAINERS="no" \
 -v /var/run/docker.sock:/var/run/docker.sock \
 $AQUA_REPO/agent:$AQUA_VERSION
+echo "step end: aqua agent"
