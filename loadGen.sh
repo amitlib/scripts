1a1831db7bf6d7cb30a7553cc604131413314574
amitlib <31591675+amitlib@users.noreply.github.com>
2018-04-02 10:41:55 +0300

    Update loadGen.sh

--- loadGen.sh
+++ loadGen.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-container_array=(alpine:2.7 nginx:latest nginx:1.12-alpine nginx:1.13 nginx:stable-alpine nginx:1.12.2-alpine alpine:2.7 alpine:3.1 alpine:3.2 alpine:3.3 alpine:3.4 alpine:3.5 alpine:3.6 alpine:3.7 alpine:latest alpine:edge ubuntu:xenial ubuntu:trusty ubuntu:zesty ubuntu:16.04 ubuntu:14.04)
+
+echo "step start: run base containers"
+docker run -d --name=grafana -p 3000:3000 grafana/grafana
+docker run --name postgres -e POSTGRES_PASSWORD=postgres -d postgres
+docker run -d --hostname $(hostname)-rabbit --name $(hostname)-rabbit rabbitmq:3
+docker run --name gcc -d -it gcc
+echo "step end: run base containers"
+
+echo "step start: run mix "
+container_array=(alpine:2.7 nginx:latest nginx:1.12-alpine nginx:1.13 nginx:stable-alpine nginx:1.12.2-alpine alpine:2.7 alpine:3.1 alpine:3.2 alpine:3.3 alpine:3.4 alpine:3.5 alpine:3.6 alpine:3.7 alpine:latest alpine:edge ubuntu:xenial ubuntu:trusty ubuntu:zesty ubuntu:16.04 ubuntu:14.04 debian fedora)
 lFlag="0"
 for i in ${container_array[@]};do
     lImageName=$(echo $i | awk -F: '{print $1}')
     docker run --rm --name ${lImageName}_${lFlag} -it -e NAME1={aqua.secret1} -e NAME2={aqua.secret2} -e AQUA_SERVICE=srv-$(hostname) -d $i
     lFlag=$(($lFlag+1))
 done
+echo "step end: run mix "
