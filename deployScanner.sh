641c476576801ab37289090d78512a2f95589472
amitlib <31591675+amitlib@users.noreply.github.com>
2018-07-08 00:03:56 +0300

    Update deployScanner.sh

--- deployScanner.sh
+++ deployScanner.sh
@@ -30,5 +30,6 @@ for i in $(seq 1 $NO_OF_SCANNERS);do
 docker run --name scanner${i}-$(hostname | awk -F'-' ' { print $NF } ') -d \
 -v /var/run/docker.sock:/var/run/docker.sock \
 $CONTAINER_REGISTRY/${AQUA_REGISTRY}/scanner-cli:${AQUA_VERSION} daemon \
---direct-cc --user administrator --password $AQUA_ADMIN_PASSWORD --host $AQUA_SERVER_IP 
+--user administrator --password $AQUA_ADMIN_PASSWORD --host $AQUA_SERVER_IP 
 done
+#--direct-cc 
