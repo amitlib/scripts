7d215c6de45fe203ae0f65bb0f84a1697709080d
amitlib <31591675+amitlib@users.noreply.github.com>
2018-07-17 15:23:45 +0300

    Update deployAquaCSP.sh

--- deployAquaCSP.sh
+++ deployAquaCSP.sh
@@ -2,19 +2,22 @@
 
 #Globals
 echo "step start: globals"
-ADMIN_USER=$1
-DOCKER_USER=$2
-DOCKER_PASS=$3
-DOCKER_REGISTRY=$4
-AQUA_IMAGE=$5
-AQUA_CONTAINER_NAME=$6
-AQUA_DB_PASSWORD=$7
-AQUA_LICENSE_TOKEN=$8
-AQUA_ADMIN_PASSWORD=$9
-INSTALL_DOCKER=${10}
+ADMIN_USER=$(whoami)
+DOCKER_USER=$1
+DOCKER_PASS=$2
+DOCKER_REGISTRY=$3
+AQUA_REPO=$4
+AQUA_VERSION=$5
+AQUA_LICENSE_TOKEN=$6
+AQUA_ADMIN_PASSWORD=$7
+INSTALL_DOCKER="${8:-no}"
 echo "step end: globals"
 
-
+echo "step start: arguments"
+echo "ADMIN_USER: $ADMIN_USER"
+echo "AQUA_ADMIN_PASSWORD: $AQUA_ADMIN_PASSWORD"
+echo "INSTALL_DOCKER: $INSTALL_DOCKER"
+echo "step end: arguments"
 
 if [ $INSTALL_DOCKER == "yes" ];then
     echo "step start: install docker-ce"
@@ -60,21 +63,21 @@ fi
 
 #Run Aqua CASP
 echo "step start: deploy Aqua CSP"
-docker run -d -p 5432:5432 -p 3622:3622 -p 8080:8080 --name $AQUA_CONTAINER_NAME \
-   -e POSTGRES_PASSWORD=${AQUA_DB_PASSWORD} \
+docker run -d -p 5432:5432 -p 3622:3622 -p 8080:8080 \
+   -e POSTGRES_PASSWORD=${AQUA_ADMIN_PASSWORD} \
    -e SCALOCK_DBUSER=postgres \
-   -e SCALOCK_DBPASSWORD=${AQUA_DB_PASSWORD} \
+   -e SCALOCK_DBPASSWORD=${AQUA_ADMIN_PASSWORD} \
    -e SCALOCK_DBNAME=scalock \
    -e SCALOCK_DBHOST=$(hostname -i) \
    -e SCALOCK_AUDIT_DBUSER=postgres \
-   -e SCALOCK_AUDIT_DBPASSWORD=${AQUA_DB_PASSWORD} \
+   -e SCALOCK_AUDIT_DBPASSWORD=${AQUA_ADMIN_PASSWORD} \
    -e SCALOCK_AUDIT_DBNAME=slk_audit \
    -e SCALOCK_AUDIT_DBHOST=$(hostname -i) \
    -e LICENSE_TOKEN=${AQUA_LICENSE_TOKEN} \
    -e ADMIN_PASSWORD=${AQUA_ADMIN_PASSWORD} \
    -v /var/lib/postgresql/data:/var/lib/postgresql/data \
    -v /var/run/docker.sock:/var/run/docker.sock \
- $AQUA_IMAGE
+ $DOCKER_REGISTRY/$AQUA_REPO/csp:$AQUA_VERSION
  
  lExitCode=$?
 if [ $lExitCode == "0" ];then
